b4a85f0d1f107b040867b5cbeeefcb48a95a892f
Alvaro Lázaro Castellón <alvarolazaro101@gmail.com>
2025-11-12 14:25:13 +0100

    Creación de las páginas de config

--- precious-time-web/src/app/app.routes.ts
+++ precious-time-web/src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { UserList } from './pages/user-list/user-list';
 import { AnalysisPage } from './pages/analysis-page/analysis-page';
 import { DashboardPage } from './pages/dashboard-page/dashboard-page';
 import { Sidebar } from './layouts/admin-layout-component/sidebar/sidebar';
+import { CategoryListPage } from './pages/configurations/category/category-list-page/category-list-page';
+import { NotificationPage } from './pages/configurations/notification-page/notification-page';
+import { SystemConfigPage } from './pages/configurations/system-config-page/system-config-page';
+import { SecurityConfigPage } from './pages/configurations/security-config-page/security-config-page';
 
 export const routes: Routes = [
     {
@@ -30,5 +34,21 @@ export const routes: Routes = [
     {
         path: 'side',
         component: Sidebar
+    },
+    {
+        path: 'config/category',
+        component: CategoryListPage
+    },
+    {
+        path: 'config/notification',
+        component: NotificationPage
+    },
+    {
+        path: 'config/system',
+        component: SystemConfigPage
+    },
+    {
+        path: 'config/security',
+        component: SecurityConfigPage
     }
 ];
---
+++ precious-time-web/src/app/pages/configurations/system-config-page/system-config-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-config-page',
+  imports: [],
+  templateUrl: './system-config-page.html',
+  styleUrl: './system-config-page.css',
+})
+export class SystemConfigPage {
+
+}
